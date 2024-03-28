ec94c246a95228957e0f77665f90199bf154d88d
João Guilherme <113355002+JGuilheerm@users.noreply.github.com>
2024-03-28 09:32:44 -0300

    :p

---
+++ blackmirror.py
@@ -0,0 +1,20 @@
+
+pergunta = input('Digite sua pergunta: ')
+
+if pergunta == 'QUAL É O NOME COMPLETO DA PROTAGONISTA DO EPISÓDIO?':
+    print("Joan Tait")
+elif pergunta == 'QUEM DIRIGE A VIDA DE JOAN EM TEMPO REAL PARA UMA SÉRIE DE TELEVISÃO?':
+    print("Um computador Quantico.")
+elif pergunta == 'QUAL É O NOME DO SERVIÇO DE STREAMING FICTÍCIO QUE PARODIA A NETFLIX NO EPISÓDIO?':
+    print ("Streamberry.")
+elif pergunta == 'COMO JOAN DESCOBRE A EXISTÊNCIA DA SÉRIE SOBRE SUA VIDA?':
+    print("Navegando pelo serviço de streaming com seu marido.")
+elif pergunta == 'QUAL É A REAÇÃO INICIAL DE JOAN AO DESCOBRIR A SÉRIE E O QUE ELA FAZ EM RESPOSTA?':
+    print ("Em choque e confusa. E tenta processar por violação de privacidade e calunia.")
+elif pergunta == 'QUAIS SÃO OS TEMAS PRINCIPAIS EXPLORADOS NESTE EPISÓDIO DE BLACK MIRROR?':
+    print ("Coletada de dados, invasão de privacidade, moraralidade e etica.")
+elif pergunta == 'O EPISÓDIO TERMINA DE MANEIRA TÍPICA PARA A SÉRIE BLACKMIRROR? EXPLIQUE.':
+    print ("Sim, termina como episódio típico da série. Ambiguo e aberto a intepretação do telespectador")
+else:
+    print("Pergunta invalida! Tente novamente.")
+
